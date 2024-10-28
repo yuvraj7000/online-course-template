4717caf535004c7d7f72a54a4c4348c0ac9394a6
Yuvraj Yadav <yuvraj7000raju@gmail.com>
2024-10-28 08:34:37 +0530

    Refactor CORS configuration to allow only specific origins

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,19 @@ config();
 
 const app = express();
 
+const allowedOrigins = ['https://online-course-template-theta.vercel.app'];
 
-app.use(cors());
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
+app.use(cors(corsOptions));
 
 app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT} PORT`));
 
